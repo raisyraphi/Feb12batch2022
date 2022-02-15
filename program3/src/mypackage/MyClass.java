4b90e342eca7792b755282c6b929ec8e7bb173e4
raisy raphi <raisyraphi@gmail.com>
2022-02-15 12:45:39 +0530

    program 3 added

---
+++ program3/src/mypackage/MyClass.java
@@ -0,0 +1,22 @@
+
+
+package mypackage;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num=10;
+		
+		/*System.out.println("Enter an Integer number:");
+		Scanner input = new Scanner(System.in);
+	    num = input.nextInt();*/
+
+		if(num%2==0)
+        System.out.println("even number");
+		else
+			System.out.println("odd number");
+	}
+
+}
